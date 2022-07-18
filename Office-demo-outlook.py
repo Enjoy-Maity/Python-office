5407643f0fe3303154af688011109c216bb9423d
Enjoy-Maity <enjoymaity@gmail.com>
2022-07-18 16:18:32 +0530

    added new outlookmail automation python script

---
+++ Office-demo-outlook.py
@@ -0,0 +1,16 @@
+import win32com.client as win32
+olapp = win32.Dispatch('Outlook.Application')
+olNS = olapp.GetNameSpace('MAPI')
+
+mailItem = olapp.CreateItem(0) #creating mailitem
+mailItem.Subject = 'TEST'
+mailItem.Body = '''Hello there
+
+regards 
+Enjoy Maity
+'''
+mailItem.to = "enjoymaity@gmail.com"
+mailItem._oleobj_.Invoke(*(64209,0,8,0, olNS.Accounts.Item('enjoy.maity@ericsson.com')))
+mailItem.Display()
+mailItem.Save()
+mailItem.Send()
