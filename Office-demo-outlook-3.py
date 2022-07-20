f7a7907ac1d03c200cf96351db1d55929f124344
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2022-07-20 15:01:54 +0530

    Update Office-demo-outlook-3.py

--- Office-demo-outlook-3.py
+++ Office-demo-outlook-3.py
@@ -1,5 +1,7 @@
+## importing the necessary modules
 import os
 os.system("pip install pywin32 pandas")
-import win32com.client as win32
-import pandas as pd
+import win32com.client as win32 # for using COM model for MS Office 365 application
+import pandas as pd # to read and manipulate the xcel sheet
+
 
