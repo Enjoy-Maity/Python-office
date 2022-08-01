e492815aacff723cacc63301c83b0bb957efe7cd
Enjoy-Maity <64701616+Enjoy-Maity@users.noreply.github.com>
2022-08-01 21:52:06 +0530

    Splitting the Office-python-outlook.py according to tasks.

--- PSCoreCSCoreRAN.py
+++ PSCoreCSCoreRAN.py
@@ -1,10 +1,3 @@
-#####################################################################
-######################### Custom Exceptions #########################
-#####################################################################
-
-from datetime import date
-
-
 class EmptyString (Exception):
     def __init__(self,msg):
         self.msg=msg
@@ -17,6 +10,7 @@ class ContainsInteger(Exception):
 #####################################################################
 #############################    Sendmail   #########################
 #####################################################################
+
 
 def sendmail(dataframe,to,cc,body,subject,sender):
     outlook_mailer=win32.Dispatch('Outlook.Application')
@@ -248,53 +242,8 @@ def paco_cscore(workbook,sender):
             </html>
         """
         sendmail(dataframe,to,cc,mpbn_html_body,subject,sender)
+        print(f"\nMail sent for {i}")
 
-    """
-    # this is one way of writing into the excel sheet but it didn't work
-
-    excel_wb=win32.gencache.EnsureDispatch("Excel.Application")
-    wb=excel_wb.Workbooks.Open(Workbook)
-    #for i in range()
-    #print(wb.Sheets(1).Name)
-    
-    ws=wb.Sheets(sheetname)
-    used=ws.UsedRange
-    print(used.Row+used.Rows.Count-2)
-    startrow=used.Row+used.Rows.Count-1
-    startcol=1
-    ws=wb.Worksheets(sheetname)
-    ws.Range(ws.Cells(startrow,startcol),ws.Cells(startrow+len(df.index)-1,startcol+len(df.columns)-1)).Value=df.values
-    wb.SaveAs("MPBN Daily Planning Sheet")
-    wb.Close()
-    
-    """
-    """
-    # another alternate way to write excel but it didn't work
-    writer=pd.ExcelWriter(Workbook,mode='a',engine='openpyxl',if_sheet_exists='replace')
-    df.to_excel(writer,sheet_name=sheetname,index=False)
-    writer.save()
-    """
-    """
-    sheet_mapping={sheetname:df}
-    with xw.App(visible=False) as app:
-        wb=app.books.open(Workbook)
-
-        current_sheets =[sheet.name for sheet in wb.sheets]
-        print(wb.sheets.count)
-
-        for sheet_name in sheet_mapping.keys():
-            if sheet_name in current_sheets:
-                wb.sheets(sheet_name).range('A1').value = sheet_mapping.get(sheet_name)    
-            
-            else:
-                new_sheet=wb.sheets.add(after=wb.sheets.count)
-                new_sheet.range('A1').value = sheet_mapping.get(sheet_name)
-                new_sheet.name=sheet_name
-            
-        wb.save()
-        wb.close()
-    """
-    
     writer=pd.ExcelWriter(workbook,engine='xlsxwriter')
 
     daily_plan_sheet.to_excel(writer,sheet_name='Planning Sheet',index=False)
@@ -328,112 +277,6 @@ def paco_cscore(workbook,sender):
     
     writer.save()
 
-#####################################################################
-############################# Fetch-details #########################
-#####################################################################
-
-def fetch_details(sender):
-    user=subprocess.getoutput("echo %username%") # finding the Username of the user where the directory of the file is located 
-
-    workbook=r"C:\Users\{}\Daily\MPBN Daily Planning Sheet.xlsx".format(user)
-    excel=pd.ExcelFile(workbook)
-    daily_plan_sheet=pd.read_excel(excel,'Planning Sheet')
-    Email_ID=pd.read_excel(excel,'Mail Id')
-
-    for j in range(0,len(daily_plan_sheet)):
-        temp=daily_plan_sheet.at[j,'Circle']
-        daily_plan_sheet.at[j,'Circle']=temp.upper()
-
-    circles=daily_plan_sheet['Circle'].unique()
-    print(circles)
-    email_id_list=Email_ID['Circle'].unique()
-    print(email_id_list)
-    # print(circles) # checking for all the unique values of circles in the MPBN Planning Sheets
-    remainder=list(set(circles)-set(email_id_list))
-    remainder_list=",".join(remainder)
-    if len(remainder)>0:
-        print(f"\nMail could not be sent for {remainder_list} as there's no email id present for the {remainder_list} in the Email ID sheet in MPBN Daily Planning Sheet")
-    
-    circles=list(set(circles)-set(remainder))
-        
-    for i in range(0,len(circles)):
-
-        execution_date=[]       #  list for collecting execution date of each Cr
-        circle=[]               #  list for collecting circle of each CR
-        maintenance_window=[]   #  list for collecting the maintenance window of each CR
-        cr_no=[]                #  list for collecting the CR No
-        activity_title=[]       #  list for collecting the activity title each CR
-        risk=[]                 #  list for collecting the risk level of each CR
-        location=[]             #  list for collecting the location of each CR
-
-        for j in range(0,len(daily_plan_sheet)):
-
-            tomorrow=datetime.now()+timedelta(1)
-
-            if daily_plan_sheet.iloc[j]['Circle']==circles[i]: # Adding constraint to check for CRs for next date only
-
-                execution_date.append(daily_plan_sheet.iloc[j]['Execution Date'])
-                maintenance_window.append(daily_plan_sheet.iloc[j]['Maintenance Window'])
-                cr_no.append(daily_plan_sheet.iloc[j]['CR NO'])
-                activity_title.append(daily_plan_sheet.iloc[j]['Activity Title'])
-                risk.append(daily_plan_sheet.iloc[j]['Risk'])
-                circle.append(daily_plan_sheet.iloc[j]['Circle'])
-                location.append(daily_plan_sheet.iloc[j]['Location'])
-
-        dictionary_for_insertion={'Execution Date':execution_date, 'Maintenance Window':maintenance_window, 'CR NO':cr_no, 'Activity Title':activity_title, 'Risk':risk,'Location':location,'Circle':circle}
-        dataframe=pd.DataFrame(dictionary_for_insertion)
-        dataframe.reset_index(drop=True,inplace=True)
-
-        cir=circles[i]
-
-        if cir=='DL':
-            row_to_fetch=0
-
-        elif cir=='PB':
-            row_to_fetch=1
-
-        elif cir=='HRY':
-            row_to_fetch=2
-        else :
-            pass
-
-
-        to=Email_ID.iloc[row_to_fetch]['To Mail List']
-        cc=Email_ID.iloc[row_to_fetch]['Copy Mail List']
-        
-        subject=f"ONLY FOR TEST :Connected End Nodes and their services on MPBN devices: {cir}"
-        body="""
-            <html>        
-                <body>
-                    <div><p>Hi team,</p><br><br>
-                        <p>Please confirm below points so that we will approve CR’s.<br><br></p>
-                        <p>1)  End nodes and service details are required which are running on respective MPBN device (in case of changes on Core/PACO/HLR devices ).<br></p>
-                        <p>2)  Design Maker & Checker confirmation mail need to be shared for all planned activity on Core/PACO/HLR devices.<br></p>
-                        <p>3)  KPI & Tester details need to be shared for all impacted nodes in Level-1 CR’s (SA).Also same details need to be shared for all Level-2 CR’s (NSA) with respect to changes on Core/PACO/HLR devices.<br><br></p>
-                    </div>
-                    <div>
-                        <p>{}</p>
-                    </div>
-                    <div>
-                        <p>Regards</p>
-                        <p>{}</p>
-                        <p>only for testing From Enjoy Maity</p>
-                        <p></p>
-                        </div>
-                </body>
-            </html>
-            """
-        sendmail(dataframe,to,cc,body,subject,sender)
-        print(f"\nMail Sent for the Circle {cir}")
-        time.sleep(5)
-            
-    paco_cscore(workbook,sender)
-
-
-#####################################################################
-################################# Main ##############################
-#####################################################################   
-
 if __name__=="__main__":
     
     try:
@@ -461,7 +304,7 @@ if __name__=="__main__":
         elif sender=='n' or sender=='N':
             sys.exit(0) # exiting the program
         else:
-            fetch_details(sender)
+            paco_cscore(sender)
 
     except ModuleNotFoundError:
          required_modules={'pandas','pywin32','Jinja2', 'openpyxl','numpy','xlsxwriter'}
@@ -495,6 +338,3 @@ if __name__=="__main__":
         current_file=__file__ # gets the value of current running file
         subprocess.run(['python', current_file])
         sys.exit(0)
-    
-
-   
